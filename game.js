899220737d5aff64af788cc43f97a8df54810500
JoshuaSimpers <simpersjoshua@gmail.com>
2024-11-01 12:41:28 -0500

    Added a function called getUserChoice that will return the choice the user picks based on the number they enter into the console

--- game.js
+++ game.js
@@ -20,8 +20,12 @@ function getComputerChoice()
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+console.log("Computer chooses: " + getComputerChoice());
 //the user enters 1, 2, or 3 to signify their choice of rock, paper, or scissors
+function getUserChoice()
+{
+    console.log("Enter the number for the option you would like: \n 1. Rock \n 2. Paper \n 3. Scissors")
+}
 //the code will then compare the results and check to see which side won taht round and add one to the winner's score
 //the game will then move on to the next round until a total of 5 rounds have been played
 //the winnder will be determined based on who won the most out of the 5 rounds
