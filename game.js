7b6d80887678a103c4f8ed46ea1acc44d5a9464d
JoshuaSimpers <simpersjoshua@gmail.com>
2024-11-05 13:24:09 -0600

    Modified the game code so that the js script listens for button presses, passes the id of the button through a function, then compares the results to the computer to see who won a round and outputs it onto the screen

--- game.js
+++ game.js
@@ -27,100 +27,56 @@ function getComputerChoice()
     return computerChoice;
 }
 
-//the user enters 1, 2, or 3 to signify their choice of rock, paper, or scissors
-function getUserChoice()
-{
-    do
-    {
-        playerChoice = prompt("Enter the number for the option you would like: \n 1. Rock \n 2. Paper \n 3. Scissors");
-        if(playerChoice == "1")
-        {
-            console.log("You chose Rock!");
-            playerChoice = String("Rock");
-            return playerChoice;
-        }
-        else if (playerChoice == "2")
-        {
-            console.log("You chose Paper!");
-            playerChoice = String("Paper");
-            return playerChoice;
-        }
-        else if (playerChoice == "3")
-        {
-            console.log("You chose Scissors!");
-            playerChoice = String("Scissors");
-            return playerChoice;
-        }
-        else
-        {
-            console.log("Invalid entry! Please try again!");
-        }
-    }
-    while (playerChoice != "Rock" || playerChoice != "Paper" || playerChoice != "Scissors");
-}
-
 //the code will then compare the results and check to see which side won that round and add one point to the winner's score
 function gameRound(playerChoice, computerChoice)
 {
     if (playerChoice == computerChoice)
     {
-        console.log(playerChoice + " is equal to " + computerChoice + ". Round " + roundNumber + " is a tie!");
+        results.textContent = playerChoice + " is equal to " + computerChoice + ". This round is a tie. Player: " + playerScore + " Computer: " + computerScore;
     }
     else if (playerChoice == "Rock" && computerChoice == "Scissors")
     {
-        console.log(playerChoice + " beats " + computerChoice + "! Player wins Round " + roundNumber);
         playerScore++;
+        results.textContent = playerChoice + " beats " + computerChoice + "! Player wins this round. Player: " + playerScore + " Computer: " + computerScore;
     }
     else if (playerChoice == "Rock" && computerChoice == "Paper")
     {
-        console.log(computerChoice + " beats " + playerChoice + "! Computer wins Round " + roundNumber);
         computerScore++;
+        results.textContent = computerChoice + " beats " + playerChoice + "! Computer wins this round. Player: " + playerScore + " Computer: " + computerScore;
     }
     else if (playerChoice == "Paper" && computerChoice == "Scissors")
     {
-        console.log(computerChoice + " beats " + playerChoice + "! Computer wins Round " + roundNumber);
         computerScore++;
+        results.textContent = computerChoice + " beats " + playerChoice + "! Computer wins this round. Player: " + playerScore + " Computer: " + computerScore;
     }
     else if(playerChoice == "Paper" && computerChoice == "Rock")
     {
-        console.log(playerChoice + " beats " + computerChoice + "! Player wins Round " + roundNumber);
         playerScore++;
+        results.textContent = playerChoice + " beats " + computerChoice + "! Player wins this round. Player: " + playerScore + " Computer: " + computerScore;
     }
     else if(playerChoice == "Scissors" && computerChoice == "Paper")
     {
-        console.log(playerChoice + " beats " + computerChoice + "! Player wins Round " + roundNumber);
         playerScore++;
+        results.textContent = playerChoice + " beats " + computerChoice + "! Player wins this round. Player: " + playerScore + " Computer: " + computerScore;
     }
     else if(playerChoice == "Scissors" && computerChoice == "Rock")
     {
-        console.log(computerChoice + " beats " + playerChoice + "! Computer wins Round " + roundNumber);
         computerScore++;
+        results.textContent = computerChoice + " beats " + playerChoice + "! Computer wins this round. Player: " + playerScore + " Computer: " + computerScore;
     }
+    gameOver();
 }
 
 //the game will then move on to the next round until a total of 5 rounds have been played
-function playGame()
+function gameOver()
 {
-    /*
-    for (roundNumber = 1; roundNumber <= 5; roundNumber++)
+    if (playerScore >= 5)
     {
-        computerChoice = getComputerChoice();
-        playerChoice = getUserChoice();
-        gameRound(playerChoice, computerChoice);
-        console.log("The score is: Computer: " + computerScore + " Player: " + playerScore);
+        results.textContent = "The player has 5 points. The player wins!"
     }
-        */
-    if (playerScore > computerScore)
+    else if(computerScore >= 5)
     {
-        console.log("The player won more rounds out of the 5 total played. The player wins!");
-    }
-    else if(playerScore < computerScore)
-    {
-        console.log("The computer won more rounds out of the 5 total played. The computer wins!");
-    }
-    else if (playerScore == computerScore)
-    {
-        console.log("Both scores are equal out of the 5 total played. It is a tie!");
+        results.textContent = "The computer has 5 points. The computer wins!"
     }
 }
 
@@ -130,8 +86,27 @@ const rockButton = document.createElement("button");
 const paperButton = document.createElement("button");
 const scissorsButton = document.createElement("button");
 
+rockButton.id = "Rock";
+paperButton.id = "Paper";
+scissorsButton.id = "Scissors";
+
 rockButton.textContent = "ROCK";
 paperButton.textContent = "PAPER";
 scissorsButton.textContent = "SCISSORS";
 
-//the winnder will be determined based on who won the most out of the 5 rounds. This is done in the playGame() function
+container.appendChild(rockButton);
+container.appendChild(paperButton);
+container.appendChild(scissorsButton);
+
+const buttonList = document.querySelectorAll("button");
+
+buttonList.forEach((button) => 
+    {
+        button.addEventListener("click", () =>
+        {
+            gameRound(button.id, getComputerChoice());
+        });
+    });
+
+const results = document.createElement("div");
+container.appendChild(results);
